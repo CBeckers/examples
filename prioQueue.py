ba693350827bb4e3105ce8e344db1bbdcc044174
Cade Beckers <cadejbeckers@gmail.com>
2025-10-21 13:47:23 -0500

    prio queue

---
+++ prioQueue.py
@@ -0,0 +1,120 @@
+# class for an element in the queue that stores the value as well as the priority
+class QueueNode:
+    def __init__(self, value, priority):
+        self.val = value
+        self.prio = priority
+
+# class for prio queue itself
+# under the hood it is just an array that stores certain values
+# easiest to do this to have each element as a class so each is one index
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def add(self, value, priority):
+        # init the class for a element in the queue and also the index we are looking in the queue
+        node = QueueNode(value, priority)
+        i = 0
+
+        # check for empty queue (add at the front)
+        if len(self.elements) == 0:
+            self.elements.insert(i, node)
+            return
+        
+        # go to the location where we add the song based upon priority and add
+        while i < len(self.elements) and self.elements[i].prio >= priority:
+            i += 1
+        self.elements.insert(i, node)
+
+    def get(self):
+        # check if queue is empty if not play the next song
+        if len(self.elements) == 0:
+            print("----- queue empty -----")
+            return
+        
+        print("playing song: " + self.elements[0].val)
+        return self.elements.pop(0).val
+    
+    # print queue
+    def print_queue(self):
+        if len(self.elements) == 0:
+            print("----- queue empty -----")
+            return
+        
+        output = "["
+        for s in self.elements:
+            output += "(value: " + str(s.val) + ", priority: " + str(s.prio) + "), "
+        print(output + "]")
+
+
+
+# examples
+if __name__ == "__main__":
+    # demo of prio queue
+    queue = PriorityQueue()
+    queue.get()
+    queue.add("Song 1", 5)
+    queue.add("Song 2", 8)
+    queue.add("Song 3", 8)
+    queue.print_queue()
+    queue.get()
+    queue.print_queue()
+    queue.get()
+    queue.print_queue()
+    queue.get()
+    queue.print_queue()
+
+
+
+
+
+
+    # spotify like demo
+
+    # now imagine songs queued by the user are priority of 1 and playlist songs are 0 
+    queue.print_queue()
+    # result
+    # 
+    # [(value: Song 1, priority: 0), (value: Song 2, priority: 0), (value: Song 3, priority: 0), 
+    # (value: Song 4, priority: 0), (value: Song 5, priority: 0), ]
+
+    # mock playlist (songs in the playlist all get put to queue)
+    queue.add("Song 1", 0)
+    queue.add("Song 2", 0)
+    queue.add("Song 3", 0)
+    queue.add("Song 4", 0)
+    queue.add("Song 5", 0)
+    queue.print_queue()
+
+    # now manually queue songs, in this case that means add a song but put it with priority of manually added songs
+
+    queue.add("Song 6", 1)
+    queue.add("Song 7", 1)
+    queue.print_queue()
+    # result 
+    #
+    # [(value: Song 6, priority: 1), (value: Song 7, priority: 1), (value: Song 1, priority: 0), 
+    # (value: Song 2, priority: 0), (value: Song 3, priority: 0), (value: Song 4, priority: 0), (value: Song 5, priority: 0), ]
+
+    # now a live demo we will play 4 songs, then add 2 more to queue manually, then play the rest
+    # this will result in the 2 manual songs and 2 queue songs to play, then add the 2 more songs to queue
+    # which will once again play those first and then the rest of the playlist songs
+    for i in range(4):
+        queue.get()
+    queue.add("Song 8", 1)
+    queue.add("Song 9", 1)
+    for i in range(6):
+        queue.get()
+    
+    # result
+    #
+    # playing song: Song 6    queue song
+    # playing song: Song 7    queue song
+    # playing song: Song 1    playlist song
+    # playing song: Song 2    playlist song
+    # playing song: Song 8    queue song
+    # playing song: Song 9    queue song
+    # playing song: Song 3    playlist song
+    # playing song: Song 4    playlist song
+    # playing song: Song 5    playlist song
+    # ----- queue empty -----
--- treeExample.py
+++ treeExample.py
@@ -3,16 +3,17 @@ class TreeNode:
         self.val = value
         self.left = None
         self.right = None
-    
-if __name__ == "__main__":
-    # tree looks like this:
-    #
-    #      4       layer 0
-    #    /   \
-    #   2     6    layer 1
-    #  / \   / \
-    # 1   3 5   7  layer 2
 
+def print_tree(node):
+    if node is None:
+        return
+    print(node.val)
+
+    # go to the next nodes recursively
+    print_tree(node.left)
+    print_tree(node.right)
+
+def set_tree():
     # layer 0
     root = TreeNode(4)
 
@@ -23,16 +24,20 @@ if __name__ == "__main__":
     # layer 2
     root.left.left = TreeNode(1)
     root.left.right = TreeNode(3)
-
     root.right.left = TreeNode(5)
     root.right.right = TreeNode(7)
 
+    return root
 
-    # how to get values
-    print(root.val)
-    print(root.left.val)
-    print(root.left.left.val)
-    print(root.left.right.val)
-    print(root.right.val)
-    print(root.right.left.val)
-    print(root.right.right.val)
+
+if __name__ == "__main__":
+    # tree looks like this:
+    #
+    #      4       layer 0
+    #    /   \
+    #   2     6    layer 1
+    #  / \   / \
+    # 1   3 5   7  layer 2
+
+    root = set_tree()
+    print_tree(root)
