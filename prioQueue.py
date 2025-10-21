076d2df57e7ed0272e7d102fe58ce5befd971886
Cade Beckers <cadejbeckers@gmail.com>
2025-10-21 14:06:08 -0500

    prio queue

--- prioQueue.py
+++ prioQueue.py
@@ -23,6 +23,7 @@ class PriorityQueue:
         
         # go to the location where we add the song based upon priority and add
         while i < len(self.elements) and self.elements[i].prio >= priority:
+
             i += 1
         self.elements.insert(i, node)
 
@@ -73,11 +74,7 @@ if __name__ == "__main__":
 
     # now imagine songs queued by the user are priority of 1 and playlist songs are 0 
     queue.print_queue()
-    # result
-    # 
-    # [(value: Song 1, priority: 0), (value: Song 2, priority: 0), (value: Song 3, priority: 0), 
-    # (value: Song 4, priority: 0), (value: Song 5, priority: 0), ]
-
+    
     # mock playlist (songs in the playlist all get put to queue)
     queue.add("Song 1", 0)
     queue.add("Song 2", 0)
@@ -85,6 +82,12 @@ if __name__ == "__main__":
     queue.add("Song 4", 0)
     queue.add("Song 5", 0)
     queue.print_queue()
+
+    # result
+    # 
+    # [(value: Song 1, priority: 0), (value: Song 2, priority: 0), (value: Song 3, priority: 0), 
+    # (value: Song 4, priority: 0), (value: Song 5, priority: 0), ]
+
 
     # now manually queue songs, in this case that means add a song but put it with priority of manually added songs
 
@@ -96,8 +99,8 @@ if __name__ == "__main__":
     # [(value: Song 6, priority: 1), (value: Song 7, priority: 1), (value: Song 1, priority: 0), 
     # (value: Song 2, priority: 0), (value: Song 3, priority: 0), (value: Song 4, priority: 0), (value: Song 5, priority: 0), ]
 
-    # now a live demo we will play 4 songs, then add 2 more to queue manually, then play the rest
-    # this will result in the 2 manual songs and 2 queue songs to play, then add the 2 more songs to queue
+    # now a live demo: we will play 4 songs, then add 2 more to queue manually, then play the rest
+    # this will result in the 2 queued songs and 2 playlist songs to play, then add the 2 more songs to queue
     # which will once again play those first and then the rest of the playlist songs
     for i in range(4):
         queue.get()
@@ -108,12 +111,12 @@ if __name__ == "__main__":
     
     # result
     #
-    # playing song: Song 6    queue song
-    # playing song: Song 7    queue song
+    # playing song: Song 6    queued song
+    # playing song: Song 7    queued song
     # playing song: Song 1    playlist song
     # playing song: Song 2    playlist song
-    # playing song: Song 8    queue song
-    # playing song: Song 9    queue song
+    # playing song: Song 8    queued song
+    # playing song: Song 9    queued song
     # playing song: Song 3    playlist song
     # playing song: Song 4    playlist song
     # playing song: Song 5    playlist song
